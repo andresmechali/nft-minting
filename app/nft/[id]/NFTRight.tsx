75dccb4f4842d844171f4f31bb4e83d53aa275ee
Andres Mechali <andresmechali@gmail.com>
2023-07-02 12:16:50 +0200

    add connection to metamask

--- app/nft/[id]/NFTRight.tsx
+++ app/nft/[id]/NFTRight.tsx
@@ -1,4 +1,14 @@
+// "use client";
+
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+
 export default function NFTRight() {
+  // Auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+  // ---
+
   return (
     <div className="flex flex-1 flex-col p-12 lg:col-span-6">
       {/*  Header */}
@@ -10,12 +20,24 @@ export default function NFTRight() {
           </span>{" "}
           NFT Market Place
         </h1>
-        <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3">
-          Sign In
+        <button
+          className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3"
+          onClick={() => {
+            address ? disconnect() : connectWithMetamask();
+          }}
+        >
+          {address ? "Sign Out" : "Sign In"}
         </button>
       </header>
 
       <hr className="my-2 border" />
+
+      {address && (
+        <p className="text-center text-sm text-rose-400">
+          You're logged in with wallet {address.substring(0, 5)}...
+          {address.substring(address.length - 5)}
+        </p>
+      )}
 
       {/* Content */}
       <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
--- app/nft/[id]/layout.tsx
+++ app/nft/[id]/layout.tsx
@@ -1,12 +1,19 @@
+"use client";
+// TODO: get this to work using SSR
+
 import React from "react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Rinkeby } from "@thirdweb-dev/chains";
 import NFTLeft from "@/app/nft/[id]/NFTLeft";
 import NFTRight from "@/app/nft/[id]/NFTRight";
 
 export default function NFTLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
-      <NFTLeft />
-      <NFTRight />
-    </div>
+    <ThirdwebProvider activeChain={Rinkeby}>
+      <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+        <NFTLeft />
+        <NFTRight />
+      </div>
+    </ThirdwebProvider>
   );
 }
